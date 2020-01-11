f13da8a51de0e09191581a49cc9b1c05782b9c4d
Safia-Safia <poolparty30@gmail.com>
2020-01-11 21:26:46 +0100

    Ajout des methode askQuestion askTemperature

--- src/com/safia/tp/Main.java
+++ src/com/safia/tp/Main.java
@@ -1,5 +1,4 @@
 package com.safia.tp;
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -12,6 +11,7 @@ public class Main {
         char reponse= ' ';
         double aConvertir=0;
         double convertit=0;
+        boolean hasError=true;
         String str1=new String(
                 "Convetisseur de degrés celcius et de degrés fahrenheit");
 
@@ -21,26 +21,16 @@ public class Main {
 
 
         do { //Boucle principale
-            do {
-                System.out.println("Choisissez le mode de convertion:\n");
-                System.out.println("1-Convertisseur Celsius-Fahrenheit");
-                System.out.println("2-Convertisseur Fahrenheit-Celsius");
-                mode=sc.nextLine().charAt(0);
+            String question= "Choisissez le mode de convertion:\n"+"1-Convertisseur Celsius-Fahrenheit\n"+"2-Convertisseur Fahrenheit-Celsius";
+            mode=askQuestion(question,'1','2');
 
-                if (mode != '1' && mode !='2') {
-                    System.out.println("Mode inconnus, veuillez choisir entre le mode 1 et le mode 2.");
-                }
-            } while (mode !='1' && mode !='2');
+            //Température à convertir
+            String question2= "Température à convertir";
+            aConvertir= askTemperature(question2,0);
 
 
-            System.out.println("Température à convertir :");
-                        try {
-                            aConvertir = sc.nextDouble();
 
-                            sc.nextLine();
-
-
-                //Mode de conversion
+            //Mode de conversion
             if(mode == '1'){//Celcius to Fahrenheit
                 convertit = ((9.0/5.0) * aConvertir) + 32.0;
                 System.out.println(aConvertir + " °C correspond à : "+ convertit+ " °F.\n");
@@ -50,19 +40,46 @@ public class Main {
                 System.out.println(aConvertir + " °F correspond à : "+ convertit + " °C.\n");
             }
 
-            //Fin du try
-                        } catch (InputMismatchException e) {
-                            System.out.println("Erreur, veuillez uniquement saisir des chiffres.\n");
-                        }
-
-
-                do { System.out.println("Souhaitez-vous convertir une autre température ? (O/N)");
-                    reponse=sc.nextLine().charAt(0);
-
-                } while (reponse !='O' && reponse !='N');
+            reponse=askQuestion("Souhaitez-vous convertir une autre température ? (O/N)", 'O', 'N');
 
         }while(reponse == 'O' );
 
         System.out.println("Au revoir !");
-    }//FIn du programme
+    }
+    /*methode poser question
+    variable repoonse, scanner, questions, reponse autorisée
+     */
+    public static char askQuestion (String question, char reponse1, char reponse2){
+        Scanner sc =new Scanner(System.in);
+        char reponseChoisie= ' ';
+        do{
+            System.out.println(question);
+            try{
+                reponseChoisie= sc.nextLine().charAt(0);
+            }catch (StringIndexOutOfBoundsException e){
+                System.out.println("Veuillez réessayer.");
+            }
+
+        }while(reponseChoisie!= reponse1 && reponseChoisie != reponse2);
+        return reponseChoisie;
+    }
+
+
+    public static double askTemperature (String question2,double temperatureChoisie){
+        Scanner sc=new Scanner(System.in);
+        boolean hasError= true;
+
+        do{
+            System.out.println(question2);
+            try {
+                temperatureChoisie = sc.nextDouble();
+                hasError=false;
+            } catch (InputMismatchException e) {
+                System.out.println("Erreur, veuillez uniquement saisir des chiffres.\n");
+            }
+            sc.nextLine();
+        }while(hasError);
+        hasError=true;
+        return temperatureChoisie;
+    }
 }//fermeture main class
